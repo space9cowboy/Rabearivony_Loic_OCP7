9276199e39b5487990d20a2f7056447a357118ef
Rabearivony Loïc <122444363+space9cowboy@users.noreply.github.com>
2023-11-24 01:18:58 +0100

    connexion a la BDD

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-//Routes
+/**
+ * * Connexion à la base de données
+ */
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mon-vieux-grimoire.8l6kfra.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connexion à MongoDB réussie");
+  })
+  .catch((error) => console.error("Erreur de connexion à MongoDB:", error));
+
 module.exports = app;
